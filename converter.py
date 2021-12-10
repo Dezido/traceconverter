8e7abd32c187cd56244337f294f3edc3c9011631
Dennis Ziebart <ziebartdennis@gmail.com>
2021-12-10 23:17:31 +0100

    Tracedata field now contains twodimensional list. Defined method for extracting datafield back to csv

--- converter.py
+++ converter.py
@@ -2,22 +2,33 @@ import pandas as pd
 import json
 
 
+###Removes unwanted columns from Trace###
 def convert_file(file, columns, name):
     df = pd.read_csv(file, header=0, delimiter=',')
     df.drop(df.columns[columns], axis=1, inplace=True)
     df.to_csv(name, float_format="%e", index=False, header=False)
 
 
-def get_df(file):
-    df = pd.read_csv(file,  delimiter=' ')
-    #df.drop(df.columns[columns], axis=1, inplace=True)
-    return df
+###Extrats the Tracedata into csv for ProFiDo###
+def extract_tracedata(trace, name):
+    df = pd.DataFrame()
+    for index in range(len(trace["tracebody"]["tracedata"])):
+        df.insert(loc=index, column="col", value=trace["tracebody"]["tracedata"][index], allow_duplicates=True)
+    df2 = df.copy()
+    df2.to_csv(name, float_format="%e", index=False, header=False)
 
 
+###gets the relevant columns for the tracedata lists###
 def get_tracedata(file, columns):
     df = pd.read_csv(file, header=0, delimiter=',')
     df.drop(df.columns[columns], axis=1, inplace=True)
-    return df.to_dict()
+    return df.values.tolist()
+
+
+def get_df(file):
+    df = pd.read_csv(file, delimiter=' ')
+    # df.drop(df.columns[columns], axis=1, inplace=True)
+    return df
 
 
 def print_characteristics(file):
@@ -48,20 +59,24 @@ example_trc = {"traceheader": {
 },
     "tracebody": {
         "data description": "Shows the delay during the resulting from the timing-attack",
-        "tracedata": get_tracedata('oneswarm-timing-attack-trace.csv', [0])
+        "tracedata": [[1, 2, 3, 4, 5],
+                      [6, 7, 8, 9, 10]]
     }
 }
 
-example_trc["tracebody"]["tracedata"] = open('result_oneswarm.trace', 'r').read().split('\n')
-example_trc["traceheader"]["statistical characteristics"]["mean"] = example_trc["tracebody"]["tracedata"]
+# example_trc["tracebody"]["tracedata"] = open('result_oneswarm.trace', 'r').read().split('\n')
 
-#print(example_trc["tracebody"]["tracedata"])
 
-get_df('result_oneswarm.trace')
+# print(len(example_trc["tracebody"]["tracedata"]))
+# extract_tracedata(example_trc, 'ex_dat.trace')
 
-with open('result.json', 'w') as fp:
-    json.dump(example_trc, fp, indent=4)
+# print(example_trc["tracebody"]["tracedata"])
+# get_df('result_oneswarm.trace')
+
+# with open('result.json', 'w') as fp:
+#    json.dump(example_trc, fp, indent=4)
 
 # convert_file('oneswarm-timing-attack-trace.csv', [0], 'result_oneswarm.trace')
-# convert_file('ask.csv', [0,3,4,5,6,7,8,], 'result_ask.trace')
+# convert_file('ask.csv', [0,3,4,5,6,7,8,], 'result_ask_cr.trace')
+print(get_tracedata('test.csv', [0, 3, 4]))
 # print_characteristics('result_oneswarm.trace')
