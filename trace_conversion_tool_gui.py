b08df38f45941ae4c57cc17bd092f5064398c371
Dennis Ziebart <ziebartdennis@gmail.com>
2022-03-29 15:33:40 +0200

    Renamed files and added lecense header

--- trace_conversion_tool_gui.py
+++ trace_conversion_tool_gui.py
@@ -1,3 +1,21 @@
+"""
+    This file is part of TraceConversionTool.
+    Copyright (c) 2022 Dennis Ziebart.
+
+    TraceConversionTool is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TraceConversionTool is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TraceConversionTool.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import configparser
 import json
 import os
@@ -11,7 +29,7 @@ from tkinter import ttk, scrolledtext
 
 import pandas as pd
 
-import trace_converting_tool_model as model
+import trace_conversion_tool_model as model
 
 # Load config file
 config = configparser.RawConfigParser()
